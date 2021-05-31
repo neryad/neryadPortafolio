63db260005feef80b613a97a696b8afeca0c390e
neryad <dayern.gomez@hotmail.com>
2021-05-31 11:14:10 -0400

    new refactor to ui on componetes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoPageComponent } from './components/no-page/no-page.component';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [NoPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ComponentsModule],
   exports: [NoPageComponent],
 })
 export class SharedModule {}
