5b12e9a4007e51f7336c850d94989e6b90739876
neryad <dayern.gomez@hotmail.com>
2021-06-01 16:55:29 -0400

    addning proyects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  projects = [
+    {
+      name: 'PocketList',
+      subTitle: '',
+      description: 'descpPoc.texto',
+      img: '../../../assets/img/projects/Pokelist.png',
+      link: 'https://neryad.github.io/pocketPage/',
+    },
+    {
+      name: 'TelePhoneCheck',
+      subTitle: '',
+      description: 'descTelphone.texto',
+      img: '../../../assets/img/projects/telphonecheck.jpg',
+      link: 'https://www.npmjs.com/package/telephonecheck',
+    },
+    {
+      name: 'bryanrivas.com',
+      subTitle: '',
+      description: 'descBr.texto',
+      img: '../../../assets/img/projects/br.jpg',
+      link: 'https://bryanrivas.com/',
+    },
+  ];
+  ngOnInit(): void {}
 }
